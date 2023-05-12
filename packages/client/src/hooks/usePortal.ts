9ca260a8d6eede7b9851a2826a072273ac214d02
김민수 MINSOO KIM <alstn113@gmail.com>
2023-05-13 00:37:12 +0900

    feat(client): add usePortal jsdocs (#50)

--- packages/client/src/hooks/usePortal.ts
+++ packages/client/src/hooks/usePortal.ts
@@ -14,6 +14,18 @@ const isBrowser = (): boolean => {
   );
 };
 
+/**
+@description
+usePortal은 문서의 본문에 div 요소를 만들고 반환하는 훅입니다.
+이는 모달, 드롭다운 및 툴팁을 만드는 데 유용합니다.
+index.html에 div 요소를 추가하지 않아도 됩니다.
+@example 
+const Modal = () => {
+  const el = usePortal('modal');
+  if (!el) return null;
+  return createPortal(<Modal />, el);
+};
+ */
 const usePortal = (selectId: string): HTMLElement | null => {
   const id = `${selectId}`;
 
