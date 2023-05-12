a57a4ecca08aba9504ffe0f31786a9752cfe15b9
김민수 MINSOO KIM <alstn113@gmail.com>
2023-05-13 01:04:57 +0900

    feat(client): 노트 생성 base-ui (#50)

---
+++ packages/client/src/pages/NoteCreate/NoteCreate.styles.tsx
@@ -0,0 +1,64 @@
+import styled from '@emotion/styled';
+import { motion } from 'framer-motion';
+
+export const Cotainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  max-width: 726px;
+  margin: 0 auto;
+  width: 100%;
+  height: 100%;
+`;
+
+export const Form = styled(motion.form)`
+  width: 100%;
+  height: 500px;
+  border: none;
+`;
+
+export const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 700;
+  margin: 0;
+  padding: 20px;
+  background-color: #eaeaea;
+  border-radius: 10px 10px 0 0;
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  height: 50px;
+  border: none;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+  padding: 0 20px;
+  font-size: 16px;
+  font-weight: 700;
+  outline: none;
+`;
+
+export const Textarea = styled.textarea`
+  width: 100%;
+  height: 350px;
+  border: none;
+  padding: 20px;
+  font-size: 16px;
+  font-weight: 700;
+  outline: none;
+  resize: none;
+`;
+
+export const Button = styled.button`
+  width: 100%;
+  height: 50px;
+  background-color: #eaeaea;
+  color: #000;
+  font-size: 16px;
+  font-weight: 700;
+  border-radius: 0 0 10px 10px;
+  cursor: pointer;
+  transition: background-color 0.2s ease-in-out;
+  &:hover {
+    background-color: #d4d4d4;
+  }
+`;
--- packages/client/src/pages/NoteCreate/NoteCreate.tsx
+++ packages/client/src/pages/NoteCreate/NoteCreate.tsx
@@ -1,7 +1,26 @@
 import BaseLayout from '~/components/layouts/BaseLayout';
+import * as S from './NoteCreate.styles';
+import { useCreateNote } from '~/hooks/queries/note';
 
 const NoteCreate = () => {
-  return <BaseLayout>NoteCreate</BaseLayout>;
+  const { mutate } = useCreateNote();
+
+  return (
+    <BaseLayout>
+      <S.Cotainer>
+        <S.Form
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          <S.Title>노트 생성</S.Title>
+          <S.Input placeholder="제목을 입력하세요" />
+          <S.Textarea placeholder="내용을 입력하세요 (장식용)" />
+          <S.Button>생성하기</S.Button>
+        </S.Form>
+      </S.Cotainer>
+    </BaseLayout>
+  );
 };
 
 export default NoteCreate;
